77bff25e90f7912669a956fdf226a175c56a1ea6
Arjunt10 <atripathi@ryerson.ca>
2024-11-12 22:01:29 -0500

    Step 2 Completed Step 3 in the works.
    Need to train the model.

--- Project2.py
+++ Project2.py
@@ -25,6 +25,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 Img_width = 500
 Img_height = 500
 Img_channel = 3
+Img_shape = (Img_width, Img_height, Img_channel)
 
 "Define Train and Validation Data Directory"
 Train_direct = r"Project 2 Data\Data\train"
@@ -41,7 +42,7 @@ Train_datagenerator = ImageDataGenerator (
 
 Validation_datagenerator = ImageDataGenerator (rescale = 1./255)
 
-"Applying Augmentation to Tran & Validation datasets"
+"Applying Augmentation to Tran & Validation Datasets"
 
 Train_generator = Train_datagenerator.flow_from_directory (
     
@@ -60,6 +61,43 @@ Validation_generator = Validation_datagenerator.flow_from_directory (
     )
 
 "STEP 2: Neural Network Architecture Design"
+
+"Creating The Convolutional Base"
+
+model = models.Sequential ()
+
+"Layer 1"
+model.add(layers.Conv2D(32, (3,3), activation = 'relu', input_shape = Img_shape))
+model.add(layers.MaxPooling2D((2,2)))
+
+"Layer 2"
+
+"Need to import LeakyRelu"
+from keras.layers import LeakyRelu
+
+model.add(layers.Conv2D(64, (3,3)))
+model.add(LeakyRelu(negative_slope = 0.01))
+model.add(layers.MaxPooling2D((2,2)))
+
+"Layer 3"
+model.add(layers.Conv2D(128, (3,3), activation = 'relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+"Flatten the Output from 3D to 1D"
+model.add(layers.Flatten())
+
+"Dense Layers"
+model.add(layers.Dense(64, activation = 'relu'))
+model.add(layers.dropout(0.5))
+model.add(layers.Dense(3, activation = 'softmax'))
+
+"STEP 3: Model Hyperparameter Analysis"
+
+"Compiled Model"
+model.compile(optimizer = 'adam', loss = "categorical_crossentropy", metrics = ["accuracy"])
+
+"Model Summary"
+model.summary()
 
 
 
